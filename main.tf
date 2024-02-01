fee905eda78d2ea1bf9bb126759d968e511744df
Borsal1 <115159536+Borsal1@users.noreply.github.com>
2024-02-01 16:58:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,19 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "eu-west-3"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "hetic-groupe-9" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
     Name = "ExampleAppServerInstance"
   }
+}
+
+resource "aws_s3_bucket" "hetic-groupe-9" {
+  bucket = "bucket-mt4"
+  acl    = "private"
 }
